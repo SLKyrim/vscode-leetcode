e11e0009f7a90bd4873725dcbdba61ce4d109403
Single Long <623962644@qq.com>
2020-01-02 17:28:49 +0800

    前缀树 嵌套类（C++未完成）

--- 208.实现-trie-前缀树.py
+++ 208.实现-trie-前缀树.py
@@ -1,31 +1,61 @@
-# -*- coding: utf-8 -*-
-"""
-Created on 2019/12/27 17:07
+#
+# @lc app=leetcode.cn id=208 lang=python3
+#
+# [208] 实现 Trie (前缀树)
+#
+# https://leetcode-cn.com/problems/implement-trie-prefix-tree/description/
+#
+# algorithms
+# Medium (62.69%)
+# Likes:    172
+# Dislikes: 0
+# Total Accepted:    19.6K
+# Total Submissions: 30.9K
+# Testcase Example:  '["Trie","insert","search","search","startsWith","insert","search"]\n' + '[[],["apple"],["apple"],["app"],["app"],["app"],["app"]]'
+#
+# 实现一个 Trie (前缀树)，包含 insert, search, 和 startsWith 这三个操作。
+# 
+# 示例:
+# 
+# Trie trie = new Trie();
+# 
+# trie.insert("apple");
+# trie.search("apple");   // 返回 true
+# trie.search("app");     // 返回 false
+# trie.startsWith("app"); // 返回 true
+# trie.insert("app");   
+# trie.search("app");     // 返回 true
+# 
+# 说明:
+# 
+# 
+# 你可以假设所有的输入都是由小写字母 a-z 构成的。
+# 保证所有输入均为非空字符串。
+# 
+# 
+#
 
-
-
-Author: Long
-"""
-
+# @lc code=start
 
 class Trie:
+
     class Node:
         def __init__(self):
             self.isEnd = False
             self.links = {}
-
+            
         def containsKey(self, ch):
             return (ord(ch) - ord('a')) in self.links.keys()
 
         def put(self, ch, node):
             self.links[ord(ch) - ord('a')] = node
-
+        
         def get(self, ch):
             return self.links[ord(ch) - ord('a')]
 
         def setEnd(self):
             self.isEnd = True
-
+        
         def getEnd(self):
             return self.isEnd
 
@@ -34,7 +64,7 @@ class Trie:
         Initialize your data structure here.
         """
         self.root = self.Node()
-
+        
     def insert(self, word: str) -> None:
         """
         Inserts a word into the trie.
@@ -45,7 +75,7 @@ class Trie:
                 node.put(ch, self.Node())
             node = node.get(ch)
         node.setEnd()
-
+        
     def search(self, word: str) -> bool:
         """
         Returns if the word is in the trie.
@@ -71,9 +101,11 @@ class Trie:
             else:
                 return False
         return True
+        
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
+# @lc code=end
 
-
-if __name__ == '__main__':
-    s = Trie()
-    s.insert("apple")
-    print(s.search("apple"))
