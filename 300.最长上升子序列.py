3eeddfcb1e8afca308baedf260060c527c23d5e0
Single Long <623962644@qq.com>
2020-03-14 12:53:18 +0800

    动态规划+最长上升子序列+改错

--- 300.最长上升子序列.py
+++ 300.最长上升子序列.py
@@ -36,12 +36,16 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         n = len(nums)
+        if n == 0:
+            return 0
         dp = [1 for i in range(n)]
+        res = 1
         for i in range(n):
             for j in range(i):
                 if nums[j] < nums[i]:
                     dp[i] = max(dp[i], dp[j] + 1)
-        return dp[n - 1]
+            res = max(res, dp[i])
+        return res
                  
 # @lc code=end
 
