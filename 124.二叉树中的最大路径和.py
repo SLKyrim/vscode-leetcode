5e627e9906c40196aaf551e7ce5c4b48c9b6fdc7
Single Long <623962644@qq.com>
2020-03-14 13:13:55 +0800

    反转链表+迭代

---
+++ 124.二叉树中的最大路径和.py
@@ -0,0 +1,56 @@
+#
+# @lc app=leetcode.cn id=124 lang=python3
+#
+# [124] 二叉树中的最大路径和
+#
+# https://leetcode-cn.com/problems/binary-tree-maximum-path-sum/description/
+#
+# algorithms
+# Hard (38.47%)
+# Likes:    344
+# Dislikes: 0
+# Total Accepted:    28K
+# Total Submissions: 70.6K
+# Testcase Example:  '[1,2,3]'
+#
+# 给定一个非空二叉树，返回其最大路径和。
+# 
+# 本题中，路径被定义为一条从树中任意节点出发，达到任意节点的序列。该路径至少包含一个节点，且不一定经过根节点。
+# 
+# 示例 1:
+# 
+# 输入: [1,2,3]
+# 
+# ⁠      1
+# ⁠     / \
+# ⁠    2   3
+# 
+# 输出: 6
+# 
+# 
+# 示例 2:
+# 
+# 输入: [-10,9,20,null,null,15,7]
+# 
+# -10
+# / \
+# 9  20
+# /  \
+# 15   7
+# 
+# 输出: 42
+# 
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def maxPathSum(self, root: TreeNode) -> int:
+# @lc code=end
+
---
+++ 206.反转链表.py
@@ -0,0 +1,47 @@
+#
+# @lc app=leetcode.cn id=206 lang=python3
+#
+# [206] 反转链表
+#
+# https://leetcode-cn.com/problems/reverse-linked-list/description/
+#
+# algorithms
+# Easy (66.06%)
+# Likes:    838
+# Dislikes: 0
+# Total Accepted:    188.1K
+# Total Submissions: 276.5K
+# Testcase Example:  '[1,2,3,4,5]'
+#
+# 反转一个单链表。
+# 
+# 示例:
+# 
+# 输入: 1->2->3->4->5->NULL
+# 输出: 5->4->3->2->1->NULL
+# 
+# 进阶:
+# 你可以迭代或递归地反转链表。你能否用两种方法解决这道题？
+# 
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        cur = head
+        pre = None
+        while cur:
+            tmp = cur.next
+            cur.next = pre
+            pre = cur
+            cur = tmp
+        return pre
+
+# @lc code=end
+
